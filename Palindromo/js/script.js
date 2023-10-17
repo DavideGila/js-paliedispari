07b0d2a7fcbda888850fccff86e4a1dd9085ed10
DavideGila <davide.gila@gmail.com>
2023-10-17 15:23:11 +0200

    created a function to the button

---
+++ Palindromo/js/script.js
@@ -0,0 +1,11 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+alert('Inserisci una parola!');
+
+const btn = document.querySelector('button');
+    
+btn.addEventListener('click', function(){
+    const inputEl = document.getElementById('text').value;
+    console.log(inputEl)
+})
